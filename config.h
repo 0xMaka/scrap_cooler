a0ee729ca0c328c42eedf118c1d658f0c750dda8
Maka <makagucci@gmx.com>
2024-04-25 21:10:19 +0100

    [+] config.h

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 // lcd device address (0x27/0x3f depending on component)
-const int LCD_ADDR = 0x27;
+const int LCD_ADDR = 0x27;  // unused currently
 
 const int BUTTON = 1;
 const int FAN = 2;
@@ -11,5 +11,5 @@ const int SCL_PIN = 5;
 const int TOP_ROW = 0;
 const int BOTTOM_ROW = 1; 
 
-const int LAZY_DELAY = 250; // replace with interrupt;
+const int LAZY_DELAY = 250; // replace with interrupt
 #endif
